536ebf26f86200930642372deb049fee70772e92
Ellie <kittens@wobble.ninja>
2020-05-14 19:50:31 +0200

    Revamp Dockerfile to be able to run tests

--- tools/compile-docker.py
+++ tools/compile-docker.py
@@ -3,9 +3,17 @@
 import os
 import shutil
 import subprocess
+import sys
 
 IMAGE_LBL = "horse3d-3hg290g"
 CONTAINER_LBL = "horse3d-r0y30i4jioog"
+RUN_TESTS = False
+
+if "--help" in sys.argv[1:]:
+    print("Runs build in docker. Use --run-tests to also run tests.")
+    sys.exit(0)
+if "--run-tests" in sys.argv[1:]:
+    RUN_TESTS = True
 
 tools_dir = os.path.abspath(os.path.dirname(__file__))
 os.chdir(os.path.join(os.path.dirname(__file__), ".."))
@@ -25,19 +33,33 @@ for container in containers:
     )
 if os.path.exists(".dockerignore"):
     os.remove(".dockerignore")
-shutil.copyfile(os.path.join(tools_dir, "dockerignore"), ".dockerignore")
+shutil.copyfile(
+    os.path.join(tools_dir, "dockerignore"),
+    ".dockerignore"
+)
 try:
     subprocess.run(
         ["docker", "build", "-t", IMAGE_LBL, "-f",
-         os.path.join(tools_dir, "Dockerfile"), "."],
+        os.path.join(tools_dir, "Dockerfile"), "."],
         stderr=subprocess.STDOUT
     ).check_returncode()
 finally:
     if os.path.exists(".dockerignore"):
         os.remove(".dockerignore")
+if not os.path.exists("binaries"):
+    os.mkdir("binaries")
 subprocess.run(
     ["docker", "run", "--label", CONTAINER_LBL,
-     "-t", "-i", "-v",
-     os.path.abspath("./releases/") + ":/compile-tree/releases/:rw,z", IMAGE_LBL],
+    "-e", "RUN_TESTS=" + ("yes" if RUN_TESTS else "no"),
+    "-t", "-i", "-v",
+    os.path.abspath("./binaries/") +
+    ":/compile-tree/binaries/:rw,z", IMAGE_LBL],
     stderr=subprocess.STDOUT
 ).check_returncode()
+
+for binaryname in os.listdir("binaries"):
+    if os.path.exists(binaryname):
+        if os.path.isdir(binaryname):
+            continue
+        os.remove(binaryname)
+    shutil.copyfile(os.path.join("binaries", binaryname), binaryname)
