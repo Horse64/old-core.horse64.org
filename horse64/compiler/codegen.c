4593f5864570fd8dbae2b4575c7bf1bea71d1993
Ellie <kittens@wobble.ninja>
2020-06-14 03:32:51 +0200

    Start visitation to do actual code gen

--- horse64/compiler/codegen.c
+++ horse64/compiler/codegen.c
@@ -4,15 +4,32 @@
 
 #include "compiler/ast.h"
 #include "compiler/astparser.h"
+#include "compiler/asttransform.h"
 #include "compiler/codegen.h"
 #include "compiler/compileproject.h"
 
+int _codegencallback_DoCodegen_visit_out(
+        h64expression *expr, h64expression *parent, void *ud
+        ) {
+    asttransforminfo *rinfo = (asttransforminfo *)ud;
 
+    return 1;
+}
 
 int codegen_GenerateBytecodeForFile(
         h64compileproject *project, h64ast *resolved_ast
         ) {
     if (!project || !resolved_ast)
         return 0;
+
+    // Do actual codegen step:
+    int transformresult = asttransform_Apply(
+        project, resolved_ast,
+        NULL, &_codegencallback_DoCodegen_visit_out,
+        NULL
+    );
+    if (!transformresult)
+        return 0;
+
     return 1;
 }
