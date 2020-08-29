25703bcd3ee0ef40c5aded6dfb602394a60bb96f
Ellie <kittens@wobble.ninja>
2020-08-30 01:25:49 +0200

    Add print handling of lists
    DCO-1.1-Signed-off-by: Ellie <kittens@wobble.ninja>

--- horse64/corelib/moduleless.c
+++ horse64/corelib/moduleless.c
@@ -8,6 +8,7 @@
 #include <alloca.h>
 #endif
 #include <assert.h>
+#include <inttypes.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
@@ -16,6 +17,7 @@
 #include "corelib/errors.h"
 #include "corelib/moduleless.h"
 #include "gcvalue.h"
+#include "hash.h"
 #include "stack.h"
 #include "unicode.h"
 #include "vmexec.h"
@@ -49,6 +51,149 @@ int corelib_containeradd(  // $$builtin.$$containeradd
     return 1;
 }
 
+struct _printvalue_seeninfo {
+    hashset *seen;
+};
+
+static int _corelib_printvalue(
+        h64vmthread *vmthread,
+        valuecontent *c, struct _printvalue_seeninfo *sinfo
+        ) {
+    char *buf = alloca(256);
+    uint64_t buflen = 256;
+    int buffree = 0;
+    switch (c->type) {
+        case H64VALTYPE_GCVAL: {
+            h64gcvalue *gcval = c->ptr_value;
+            switch (gcval->type) {
+                case H64GCVALUETYPE_STRING: {
+                    int allocfail = 0;
+                    if (buflen < gcval->str_val.len * 4 + 1) {
+                        char *newbuf = malloc(
+                            gcval->str_val.len * 4 + 1
+                        );
+                        buffree = 1;
+                        if (newbuf) {
+                            if (buffree)
+                                free(buf);
+                            buf = newbuf;
+                            buflen = gcval->str_val.len * 4 + 1;
+                        } else {
+                            allocfail = 1;
+                        }
+                    }
+                    int64_t outlen = 0;
+                    int result = utf32_to_utf8(
+                        gcval->str_val.s, gcval->str_val.len,
+                        buf, buflen, &outlen, 1
+                    );
+                    if (!result) {
+                        assert(allocfail);
+                        if (buffree)
+                            free(buf);
+                        return vmexec_ReturnFuncError(
+                            vmthread, H64STDERROR_OUTOFMEMORYERROR,
+                            "alloc failure printing string"
+                        );
+                    }
+                    if (outlen >= (int64_t)buflen)
+                        outlen = buflen - 1;
+                    buf[outlen] = '\0';
+                    printf("%s", buf);
+                    break;
+                }
+                case H64GCVALUETYPE_LIST: {
+                    if (!sinfo->seen) {
+                        sinfo->seen = hashset_New(64);
+                    }
+
+                    if (hashset_Contains(
+                            sinfo->seen, &gcval->list_values,
+                            sizeof(gcval->list_values)
+                            )) {
+                        printf("...");
+                        break;
+                    }
+                    if (!hashset_Add(
+                            sinfo->seen, &gcval->list_values,
+                            sizeof(gcval->list_values)
+                            )) {
+                        if (buffree)
+                            free(buf);
+                        return vmexec_ReturnFuncError(
+                            vmthread, H64STDERROR_OUTOFMEMORYERROR,
+                            "alloc failure printing list"
+                        );
+                    }
+                    printf("[");
+                    int64_t entry_offset = -1;
+                    int64_t total_entry_count = (
+                        gcval->list_values->list_total_entry_count
+                    );
+                    listblock *block = gcval->list_values->first_block;
+                    while (block) {
+                        int64_t k = 0;
+                        while (k < block->entry_count) {
+                            entry_offset++;
+                            assert(entry_offset < total_entry_count);
+                            if (!_corelib_printvalue(
+                                    vmthread,
+                                    &block->entry_values[k], sinfo
+                                    )) {
+                                if (buffree)
+                                    free(buf);
+                                return 0;
+                            }
+                            if (likely(entry_offset + 1 <
+                                    total_entry_count)) {
+                                printf(", ");
+                            }
+                            k++;
+                        }
+                        block = block->next_block;
+                    }
+                    printf("]");
+                    break;
+                }
+                default:
+                    printf("<unhandled refvalue type=%d>\n",
+                           (int)gcval->type);
+            }
+            break;
+        }
+        case H64VALTYPE_SHORTSTR: {
+            assert(buflen >= 25);
+            assert(c->shortstr_len >= 0 &&
+                   c->shortstr_len < 5);
+            unicodechar shortstr_value[
+                VALUECONTENT_SHORTSTRLEN + 1
+            ];
+            memcpy(&shortstr_value, c->shortstr_value,
+                   VALUECONTENT_SHORTSTRLEN + 1);
+            int64_t outlen = 0;
+            int result = utf32_to_utf8(
+                shortstr_value, c->shortstr_len,
+                buf, 25, &outlen, 1
+            );
+            assert(result != 0 && outlen > 0 && outlen + 1 < 25);
+            buf[outlen] = '\0';
+            printf("%s", buf);
+            break;
+        }
+        case H64VALTYPE_INT64: {
+            printf("%" PRId64, c->int_value);
+            break;
+        }
+        default: {
+            printf("<unhandled valuecontent type=%d>\n", (int)c->type);
+            break;
+        }
+    }
+    if (buffree)
+        free(buf);
+    return 1;
+}
+
 int corelib_print(  // $$builtin.print
         h64vmthread *vmthread
         ) {
@@ -70,74 +215,11 @@ int corelib_print(  // $$builtin.print
             printf(" ");
         valuecontent *c = vmlist_Get(l, i + 1);
         assert(c != NULL);
-        switch (c->type) {
-            case H64VALTYPE_GCVAL: {
-                h64gcvalue *gcval = c->ptr_value;
-                switch (gcval->type) {
-                    case H64GCVALUETYPE_STRING: {
-                        int allocfail = 0;
-                        if (buflen < gcval->str_val.len * 4 + 1) {
-                            char *newbuf = malloc(
-                                gcval->str_val.len * 4 + 1
-                            );
-                            buffree = 1;
-                            if (newbuf) {
-                                if (buffree)
-                                    free(buf);
-                                buf = newbuf;
-                                buflen = gcval->str_val.len * 4 + 1;
-                            } else {
-                                allocfail = 1;
-                            }
-                        }
-                        int64_t outlen = 0;
-                        int result = utf32_to_utf8(
-                            gcval->str_val.s, gcval->str_val.len,
-                            buf, buflen, &outlen, 1
-                        );
-                        if (!result) {
-                            assert(allocfail);
-                            return vmexec_ReturnFuncError(
-                                vmthread, H64STDERROR_OUTOFMEMORYERROR,
-                                "alloc failure printing string"
-                            );
-                        }
-                        if (outlen >= (int64_t)buflen)
-                            outlen = buflen - 1;
-                        buf[outlen] = '\0';
-                        printf("%s", buf);
-                        break;
-                    }
-                    default:
-                        printf("<unhandled refvalue type=%d>\n",
-                               (int)gcval->type);
-                }
-                break;
-            }
-            case H64VALTYPE_SHORTSTR: {
-                assert(buflen >= 25);
-                assert(c->shortstr_len >= 0 &&
-                       c->shortstr_len < 5);
-                unicodechar shortstr_value[
-                    VALUECONTENT_SHORTSTRLEN + 1
-                ];
-                memcpy(&shortstr_value, c->shortstr_value,
-                       VALUECONTENT_SHORTSTRLEN + 1);
-                int64_t outlen = 0;
-                int result = utf32_to_utf8(
-                    shortstr_value, c->shortstr_len,
-                    buf, 25, &outlen, 1
-                );
-                assert(result != 0 && outlen > 0 && outlen + 1 < 25);
-                buf[outlen] = '\0';
-                printf("%s", buf);
-                break;
-            }
-            default: {
-                printf("<unhandled valuecontent type=%d>\n", (int)c->type);
-                break;
-            }
-        }
+        struct _printvalue_seeninfo sinfo = {0};
+        if (!_corelib_printvalue(vmthread, c, &sinfo))
+            return 0;  // error raised
+        if (sinfo.seen)
+            hashset_Free(sinfo.seen);
         i++;
     }
     printf("\n");
