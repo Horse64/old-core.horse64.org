064e46b6715bae864132c1dc9e42f8e7d082eb61
Ellie <kittens@wobble.ninja>
2020-10-01 09:28:51 +0200

    Fix locale creation on Windows
    DCO-1.1-Signed-off-by: Ellie <kittens@wobble.ninja>

--- horse64/nonlocale.c
+++ horse64/nonlocale.c
@@ -15,9 +15,13 @@ locale_t h64locale = (locale_t) 0;
 
 __attribute__((constructor)) static inline void _genlocale() {
     if (!h64localeset) {
+        #if defined(_WIN64) || defined(_WIN32)
+        h64locale = _create_locale(LC_ALL, "C");
+        #else
         h64locale = newlocale(
             LC_ALL, "C", (locale_t) 0
         );
+        #endif
         if (h64locale == (locale_t) 0) {
             fprintf(stderr, "failed to generate locale\n");
             _exit(1);
