f08753578d5c57a288b77097c16dc4716f26127e
Ellie <kittens@wobble.ninja>
2021-01-24 08:48:31 +0100

    Add missing preinit files
    DCO-1.1-Signed-off-by: Ellie <kittens@wobble.ninja>

---
+++ horse64/mainpreinit.c
@@ -0,0 +1,50 @@
+// Copyright (c) 2020-2021, ellie/@ell1e & Horse64 Team (see AUTHORS.md),
+// also see LICENSE.md file.
+// SPDX-License-Identifier: BSD-2-Clause
+
+
+#include "compileconfig.h"
+
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#if defined(_WIN32) || defined(_WIN64)
+#include <fcntl.h>
+#include <windows.h>
+#else
+#include <signal.h>
+#endif
+
+#include "filesys.h"
+#include "mainpreinit.h"
+#include "vfs.h"
+
+void _load_unicode_data();  // widechar.c
+
+static int _didpreinit = 0;
+
+void main_PreInit() {
+    if (_didpreinit)
+        return;
+    _didpreinit = 1;
+    #if defined(_WIN32) || defined(_WIN64)
+    _setmode(_fileno(stdin), O_BINARY);
+    #else
+    signal(SIGPIPE, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+    #endif
+    char *exepath = filesys_GetOwnExecutable();
+    if (!exepath) {
+        fprintf(stderr,
+            "horse64/main.c: error: failed "
+            "to get own binary path\n");
+        exit(1);
+        return;
+    }
+    vfs_Init(exepath);
+    free(exepath);
+
+    // Load up unicode tables for widechar.c:
+    _load_unicode_data();
+}
---
+++ horse64/mainpreinit.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2020-2021, ellie/@ell1e & Horse64 Team (see AUTHORS.md),
+// also see LICENSE.md file.
+// SPDX-License-Identifier: BSD-2-Clause
+
+#ifndef HORSE64_MAINPREINIT_H_
+#define HORSE64_MAINPREINIT_H_
+
+void main_PreInit();
+
+#endif  // HORSE64_MAINPREINIT_H_
