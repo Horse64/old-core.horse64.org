979630ff626bbac53b961702173a8ddf101db2e9
Ellie <kittens@wobble.ninja>
2021-01-26 19:49:45 +0100

    Minor documentation update
    DCO-1.1-Signed-off-by: Ellie <kittens@wobble.ninja>

--- horse64/corelib/net.c
+++ horse64/corelib/net.c
@@ -260,6 +260,8 @@ int netlib_connect(h64vmthread *vmthread) {
      * @param host the host to connect to, either by name or by ip
      * @param port the port of the host to connect to
      * @param encrypt=false whether to connect using TLS/SSL encryption
+     * @raises ResourcError raised when host name resolution or connection
+     *    fails, for whatever reason (other than local out of memory).
      * @returns a @see{network stream|net.stream}
      */
     assert(STACK_TOP(vmthread->stack) >= 3);
