5072f4d7f2be9fd677ccaed5e19c4d78cf13fef8
Ellie <kittens@wobble.ninja>
2021-02-18 22:55:00 +0100

    Remove unused python script leftovers
    DCO-1.1-Signed-off-by: Ellie <kittens@wobble.ninja>

--- tools/rename-bullet-results.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-import os
-import shutil
-
-for f in os.listdir("."):
-    if "_gmake_" in f and f.endswith(".a") and not os.path.isdir(f):
-        shutil.move(f, f.partition("_gmake_")[0] + ".a")
